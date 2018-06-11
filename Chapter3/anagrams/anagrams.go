8b3ee84aec15367d2c334fd5cc38c57cb0178473
Nico <fahani45@gmail.com>
2018-06-12 00:13:06 +0200

    Exercise 3.12. Write a function that reports if two strings are anagram

---
+++ Chapter3/anagrams/anagrams.go
@@ -0,0 +1,32 @@
+// Exercise 3.12. Write a function that reports if two strings are anagram
+package main
+
+import "fmt"
+
+func areAnagram(a, b string) bool {
+	runesA, runesB := []rune(a), []rune(b)
+	lenA, lenB := len(runesA), len(runesB)
+
+	if lenA != lenB {
+		return false
+	}
+
+	for _, rA := range runesA {
+		lenB = len(runesB)
+		for i, rB := range runesB {
+			 if rA == rB {
+			 	runesB = append(runesB[:i],runesB[i+1:]...)
+			 	break
+			 }
+
+			 if lenB == i+1 {
+			 	return false
+			 }
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(areAnagram("ANAGRAM ", "GARANAMA"))
+}

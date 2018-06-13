2476153622c24ecd24f7888367f15d013bfbc40a
Nico <fahani45@gmail.com>
2018-06-13 19:34:22 +0200

    This example prints and compares the SHA256 digests of "x" and "X"

---
+++ Chapter4/sha256.go
@@ -0,0 +1,14 @@
+// This example prints and compares the SHA256 digests of "x" and "X"
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+func main() {
+	c1 := sha256.Sum256([]byte("x"))
+	c2 := sha256.Sum256([]byte("X"))
+	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
+	fmt.Printf("%b %b",[]byte("x"),[]byte("X") )
+}

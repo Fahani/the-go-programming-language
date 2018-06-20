9f3dd264341ed7e60b6fd32e188517ec6811422a
Nico <fahani45@gmail.com>
2018-06-20 19:24:28 +0200

    Dedup reads a sequence of lines and prints only the first occurrence of each distinct line

---
+++ Chapter4/dedup/dedup.go
@@ -0,0 +1,24 @@
+// Dedup reads a sequence of lines and prints only the first occurrence of each distinct line
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+)
+
+func main() {
+	seen := make(map[string]bool)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		line := input.Text()
+		if !seen[line] {
+			seen[line] = true
+			fmt.Println(line)
+		}
+	}
+	if err := input.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "dedup: %v\n", err)
+		os.Exit(1)
+	}
+}

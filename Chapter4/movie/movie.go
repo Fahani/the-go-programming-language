bc2da365b2b6988b4bf40f8ee4e3d3b4d0fe89f8
Nico <fahani45@gmail.com>
2018-07-05 20:01:03 +0200

    Playing with struct and arrays

---
+++ Chapter4/movie/movie.go
@@ -0,0 +1,24 @@
+// Playing with struct and arrays
+package main
+
+import "fmt"
+
+type Movie struct {
+	Title string
+	Year int `json:"released"`
+	Color bool `json:"color, omitempy"`
+	Actors []string
+}
+
+func main() {
+	var movies = []Movie{
+		{Title: "Casablanca", Year: 1942, Color: false,
+			Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
+		{Title: "Cool Hand Luke", Year: 1967, Color: true,
+			Actors: []string{"Paul Newman"}},
+		{Title: "Bullitt", Year: 1968, Color: true,
+			Actors: []string{"Steve McQueen", "Jacqueline Bisset"}},
+	}
+
+	fmt.Printf("%#v\n",movies)
+}
